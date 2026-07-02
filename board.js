2c54ac840b98b39244b9de661c47f481fc6d0c8d
JacksonEdwards-CSE <jacksoned0713@gmail.com>
2026-07-02 09:56:36 -0600

    updating modal card

--- board.js
+++ board.js
@@ -6,6 +6,10 @@ const modalImage = document.querySelector("#modal-img")
 const modalTitle = document.querySelector("#modal-title")
 const modalDescription = document.querySelector("#modal-description")
 
+const image = card.querySelector(".card-img");
+const title = card.querySelector(".card-title");
+const description = card.querySelector(".card-description");
+
 card.addEventListener("click", toggleModal);
 
 function toggleModal(){
@@ -27,4 +31,19 @@ modal.addEventListener('click', (event) => {
     if (event.target === modal) {
         modal.close();
     }
+});
+
+const cards = document.querySelectorAll(".card");
+
+cards.forEach(card => {
+    card.addEventListener("click", (event) => {
+        console.log("clicked")
+        const clickedCard = event.currentTarget;
+
+        modalImage.src = clickedCard.querySelector(".card-img").src;
+        modalTitle.textContent = clickedCard.querySelector(".card-title").textContent;
+        modalDescription.textContent = clickedCard.querySelector(".card-description").textContent;
+
+        modal.showModal();
+    });
 });
